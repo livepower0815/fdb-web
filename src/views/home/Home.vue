8fa9aab0305fc2b862549fe371254c72d0d7ad94
kerry_chen <kerry_chen@paradise-soft.com.tw>
2021-07-06 22:31:25 +0800

    style: 首頁info切版

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,7 @@
     <!--Banner開始-->
     <div class="banner">
       <div class="banner-title">返多寶-虛擬貨幣返佣先驅</div>
-      <div class="banner-sub">致力打造全球最大虛擬貨幣返佣平台，讓交易變得更優惠、更便利。</div>
+      <div class="banner-sub">致力打造全最大虛擬貨幣反傭平台，讓虛擬貨幣交易變得更優惠、讓消費者不在為了不透明的手續費而煩惱</div>
       <div class="banner-more">
         <div class="banner-button">了解更多</div>
       </div>
@@ -18,123 +18,11 @@
     <!--加入我們 結束-->
 
     <!--介紹 開始-->
-    <div v-if="false" class="outline info-outline">
-      <div class="outblock">
-        <div class="info-main-title">
-          <div>
-            <div class="title">返多寶可以幫你</div>
-            <div class="sub">返多寶會員特色資訊，可以達到有效管理與圖表統計</div>
-          </div>
-        </div>
-
-        <div class="info-main-pic">
-          <img id="unqi-account" src="@/assets/img/index-intro-00.jpg" alt="" />
-          <img id="bonues-visual" src="@/assets/img/index-intro-01.jpg" alt="" style="display: none" />
-          <img id="perfect-control" src="@/assets/img/index-intro-02.jpg" alt="" style="display: none" />
-          <img id="servicr-team" src="@/assets/img/index-intro-03.jpg" alt="" style="display: none" />
-          <div class="info-slider">
-            <div id="unqi-account-slide" class="active"></div>
-            <div id="bonues-visual-slide"></div>
-            <div id="perfect-control-slide"></div>
-            <div id="servicr-team-slide"></div>
-          </div>
-        </div>
-
-        <div class="info-main">
-          <div class="main">
-            <div id="unqi-account-btn" class="active">
-              <div class="title">一對多帳號視覺化管理</div>
-              <div class="sub">返多寶帳號整合多間交易所<br />輕鬆管理所有返佣資訊</div>
-            </div>
-            <div id="bonues-visual-btn">
-              <div class="title">保證多幣種返佣</div>
-              <div class="sub">不限合約類型<br />提供多種貨幣返佣</div>
-            </div>
-            <div id="perfect-control-btn">
-              <div class="title">高安全性返佣</div>
-              <div class="sub">返多寶獨立冷錢包<br />人工返佣保障會員安全性</div>
-            </div>
-            <div id="servicr-team-btn">
-              <div class="title">在線客服團隊</div>
-              <div class="sub">專業提供24小時客服服務<br />隨時隨地回應您的建議與疑問</div>
-            </div>
-          </div>
-          <a href="javascript:void(0)" class="info-btn">
-            開始使用
-            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M11 0.333328L9.12004 2.21333L16.56 9.66666H0.333374V12.3333H16.56L9.12004 19.7867L11 21.6667L21.6667 11L11 0.333328Z"
-                fill="#62FFFF"
-              />
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
+    <Info />
     <!--介紹 結束-->
 
     <!--底版 開始-->
     <div v-if="false" class="outline line-bg">
-      <!--介紹 開始-->
-      <div class="outline info-outline">
-        <div class="outblock">
-          <div class="info-main-title">
-            <div>
-              <div class="title">返多寶可以幫你</div>
-              <div class="sub">返多寶會員特色資訊，可以達到有效管理與圖表統計</div>
-            </div>
-          </div>
-
-          <div class="info-main-pic">
-            <img id="unqi-account" src="@/assets/img/index-intro-00.jpg" alt="" />
-            <img id="bonues-visual" src="@/assets/img/index-intro-01.jpg" alt="" style="display: none" />
-            <img id="perfect-control" src="@/assets/img/index-intro-02.jpg" alt="" style="display: none" />
-            <img id="servicr-team" src="@/assets/img/index-intro-03.jpg" alt="" style="display: none" />
-            <div class="info-slider">
-              <div id="unqi-account-slide" class="active"></div>
-              <div id="bonues-visual-slide"></div>
-              <div id="perfect-control-slide"></div>
-              <div id="servicr-team-slide"></div>
-            </div>
-          </div>
-
-          <div class="info-main">
-            <div class="main">
-              <div id="unqi-account-btn" class="active">
-                <div class="title">一對多帳號視覺化管理</div>
-                <div class="sub">返多寶帳號整合多間交易所<br />輕鬆管理所有返佣資訊</div>
-              </div>
-              <div id="bonues-visual-btn">
-                <div class="title">保證多幣種返佣</div>
-                <div class="sub">不限合約類型<br />提供多種貨幣返佣</div>
-              </div>
-              <div id="perfect-control-btn">
-                <div class="title">高安全性返佣</div>
-                <div class="sub">返多寶獨立冷錢包<br />人工返佣保障會員安全性</div>
-              </div>
-              <div id="servicr-team-btn">
-                <div class="title">在線客服團隊</div>
-                <div class="sub">專業提供24小時客服服務<br />隨時隨地回應您的建議與疑問</div>
-              </div>
-            </div>
-            <a href="javascript:void(0)" class="info-btn">
-              開始使用
-              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M11 0.333328L9.12004 2.21333L16.56 9.66666H0.333374V12.3333H16.56L9.12004 19.7867L11 21.6667L21.6667 11L11 0.333328Z"
-                  fill="#62FFFF"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
-      <!--介紹 結束-->
-
       <!--最新消息 開始-->
       <div class="outline news-outline">
         <div class="news-title">最新活動與消息</div>
@@ -175,60 +63,13 @@
 
 <script>
 import JoinUs from './components/JoinUs.vue'
+import Info from './components/Info.vue'
 
 export default {
   name: 'Home',
   components: {
-    JoinUs
-  },
-  mounted() {
-    const $ = window.$
-    // $('.carousel').flickity({
-    //   // options
-    //   autoPlay: true,
-    //   cellAlign: 'left',
-    //   contain: true
-    // })
-    $('#unqi-account-btn,#unqi-account-slide').click(function() {
-      $('#unqi-account-btn,#unqi-account-slide').addClass('active')
-      $(
-        '#bonues-visual-btn,#perfect-control-btn,#servicr-team-btn,#bonues-visual-slide,#perfect-control-slide,#servicr-team-slide'
-      ).removeClass('active')
-      $('#bonues-visual').hide()
-      $('#perfect-control').hide()
-      $('#servicr-team').hide()
-      $('#unqi-account').fadeIn()
-    })
-    $('#bonues-visual-btn,#bonues-visual-slide').click(function() {
-      $('#bonues-visual-btn,#bonues-visual-slide').addClass('active')
-      $(
-        '#unqi-account-btn,#perfect-control-btn,#servicr-team-btn,#unqi-account-slide,#perfect-control-slide,#servicr-team-slide'
-      ).removeClass('active')
-      $('#unqi-account').hide()
-      $('#perfect-control').hide()
-      $('#servicr-team').hide()
-      $('#bonues-visual').fadeIn()
-    })
-    $('#perfect-control-btn,#perfect-control-slide').click(function() {
-      $('#perfect-control-btn,#perfect-control-slide').addClass('active')
-      $('#unqi-account-btn,#bonues-visual-btn,#servicr-team-btn,#unqi-account-slide,#bonues-visual-slide,#servicr-team-slide').removeClass(
-        'active'
-      )
-      $('#bonues-visual').hide()
-      $('#unqi-account').hide()
-      $('#servicr-team').hide()
-      $('#perfect-control').fadeIn()
-    })
-    $('#servicr-team-btn,#servicr-team-slide').click(function() {
-      $('#servicr-team-btn,#servicr-team-slide').addClass('active')
-      $(
-        '#unqi-account-btn,#unqi-account-slide,#bonues-visual-btn,#bonues-visual-slide,#perfect-control-btn,#perfect-control-slide'
-      ).removeClass('active')
-      $('#bonues-visual').hide()
-      $('#perfect-control').hide()
-      $('#unqi-account').hide()
-      $('#servicr-team').fadeIn()
-    })
+    JoinUs,
+    Info
   }
 }
 </script>
@@ -268,7 +109,7 @@ export default {
       }
     }
     &-currencies {
-      margin-top: 38px;
+      margin-top: 100px;
       display: flex;
       justify-content: center;
       align-items: center;
---
+++ src/views/home/components/Info.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="info">
+    <div class="info-pic">
+      <transition name="fade-transform" mode="out-in">
+        <img v-if="infoSelected === 'unqi-account'" src="@/assets/img/index-intro-00.jpg" alt="unqi-account" key="unqi-account" />
+        <img v-if="infoSelected === 'bonues-visual'" src="@/assets/img/index-intro-02.jpg" alt="bonues-visual" key="bonues-visual" />
+        <img v-if="infoSelected === 'perfect-control'" src="@/assets/img/index-intro-03.jpg" alt="perfect-control" key="perfect-control" />
+        <img v-if="infoSelected === 'servicr-team'" src="@/assets/img/index-intro-01.jpg" alt="servicr-team" key="servicr-team" />
+      </transition>
+      <div class="info-pic-slider">
+        <div @click="infoSelected = 'unqi-account'" :class="{ actived: infoSelected === 'unqi-account' }"></div>
+        <div @click="infoSelected = 'bonues-visual'" :class="{ actived: infoSelected === 'bonues-visual' }"></div>
+        <div @click="infoSelected = 'perfect-control'" :class="{ actived: infoSelected === 'perfect-control' }"></div>
+        <div @click="infoSelected = 'servicr-team'" :class="{ actived: infoSelected === 'servicr-team' }"></div>
+      </div>
+    </div>
+
+    <div class="info-mainer">
+      <div class="info-mainer-title">
+        <div class="title">返多寶可以幫你</div>
+        <div class="sub">返多寶會員特色資訊，可以達到有效管理與圖表統計</div>
+      </div>
+      <div class="info-mainer-body">
+        <div class="info-mainer-body__list">
+          <div class="list-item" @click="infoSelected = 'unqi-account'" :class="{ actived: infoSelected === 'unqi-account' }">
+            <div class="title">獨立的帳號綁定</div>
+            <div class="sub">平台之間獨立帳號驗證，確保資料的安全信任度</div>
+          </div>
+          <div class="list-item" @click="infoSelected = 'bonues-visual'" :class="{ actived: infoSelected === 'bonues-visual' }">
+            <div class="title">佣金收入視覺化</div>
+            <div class="sub">清楚分類返佣種類與金額，幫助會員快速檢視返佣狀態</div>
+          </div>
+          <div class="list-item" @click="infoSelected = 'perfect-control'" :class="{ actived: infoSelected === 'perfect-control' }">
+            <div class="title">完善資訊管理</div>
+            <div class="sub">多個交易所與貨幣地址統一管理，便於會員控管資料</div>
+          </div>
+          <div class="list-item" @click="infoSelected = 'servicr-team'" :class="{ actived: infoSelected === 'servicr-team' }">
+            <div class="title">在線客服團隊</div>
+            <div class="sub">為您提供24小時溝通服務，無時不刻回應您的建議及疑問</div>
+          </div>
+        </div>
+        <a href="javascript:void(0)" class="info-btn">
+          開始使用
+          <img src="@/assets/img/home/array-right.png" alt="" style="width: auto; margin-left: 10px;" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      infoSelected: 'unqi-account'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.info {
+  display: flex;
+  padding: 0 5%;
+  &-pic {
+    padding: 0 3%;
+    flex: 1;
+    position: relative;
+    width: 100%;
+    height: 560px;
+    display: flex;
+    justify-content: center;
+    img {
+      width: 90%;
+    }
+    &-slider {
+      position: absolute;
+      bottom: 20px;
+      right: 0;
+      left: 0;
+      display: flex;
+      justify-content: center;
+      > div {
+        width: 34px;
+        margin: 0 4px;
+        background: #cccccc80;
+        height: 9px;
+        border-radius: 100px;
+        cursor: pointer;
+        &.actived,
+        &:hover {
+          background: #62ffff;
+        }
+      }
+    }
+  }
+  &-mainer {
+    flex: 1;
+    width: 100%;
+    padding: 0 3%;
+    &-title {
+      margin-bottom: 24px;
+      .title {
+        font-weight: bold;
+        font-size: 48px;
+        line-height: 64px;
+      }
+      .sub {
+        font-size: 16px;
+        line-height: 24px;
+      }
+    }
+    &-body {
+      &__list {
+        display: flex;
+        flex-wrap: wrap;
+        margin-bottom: 24px;
+        .list-item {
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          border-radius: 8px;
+          width: calc(50% - 44px);
+          margin: 6px;
+          height: 180px;
+          padding: 0 12px 0 20px;
+          cursor: pointer;
+          &.actived,
+          &:hover {
+            background-image: linear-gradient(to left top, #5ad5d5, #3ea9cc);
+          }
+          &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            border-radius: 8px;
+            padding: 2px;
+            background: linear-gradient(315deg, #5ad5d5 0%, #3ea9cc 100%);
+            mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+            -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+            mask-composite: exclude;
+            -webkit-mask-composite: destination-out;
+          }
+          .title {
+            font-weight: bold;
+            font-size: 24px;
+            line-height: 32px;
+            margin-bottom: 16px;
+          }
+          .sub {
+            font-size: 16px;
+            line-height: 150%;
+          }
+        }
+      }
+      .info-btn {
+        display: flex;
+        align-items: center;
+        font-weight: bold;
+        font-size: 24px;
+        color: #62ffff;
+      }
+    }
+  }
+}
+</style>
