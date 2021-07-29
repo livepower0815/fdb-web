1b19bc8991a6aa4d4da53eb5662eeb04a1bf5a75
kerry_chen <kerry_chen@paradise-soft.com.tw>
2021-07-29 20:31:14 +0800

    style: footer RWD

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -5,13 +5,13 @@
       <div class="footer-img">
         <img src="@/assets/img/nav/logo.png" alt="nav-icon" />
       </div>
-      <div v-if="deviceWidth >= 1024" class="footer-menu">
+      <div class="footer-menu">
         <router-link to="/">FDB首頁</router-link>
         <router-link to="/about">關於FDB</router-link>
         <router-link to="/partner">合作夥伴</router-link>
         <router-link to="/news">最新消息</router-link>
       </div>
-      <div v-if="deviceWidth >= 768" class="footer-connect">
+      <div class="footer-connect">
         <div>聯絡我們</div>
         <img src="@/assets/img/footer/telegram.png" alt="connect-icon" class="connect-icon" />
         <img src="@/assets/img/footer/wechat.png" alt="connect-icon" class="connect-icon" />
@@ -21,10 +21,10 @@
     <div class="footer-bottom">
       <div class="bottom-left">
         <div class="footer-copy-right">Copyright © 2020 FDB.</div>
-        <div v-if="deviceWidth >= 768" class="links">
-          <router-link to="/">免責聲明</router-link>
+        <div class="links">
+          <router-link :to="{ name: 'Disclaimer' }">免責聲明</router-link>
           <div style="width: 0.5px;height: 20px;background-color: #ccc;margin-right: 20px;"></div>
-          <router-link to="/">隱私條款</router-link>
+          <router-link :to="{ name: 'PrivacyPolicy' }">隱私條款</router-link>
         </div>
       </div>
       <div class="bottom-right">
@@ -63,8 +63,15 @@ export default {
     align-items: center;
     border-bottom: 1px solid #fff;
     padding: 20px 0;
+    @media screen and (max-width: 700px) {
+      flex-direction: column;
+      align-items: unset;
+    }
     .footer-img {
       flex: 1;
+      @media screen and (max-width: 960px) {
+        flex: 0 0 auto;
+      }
       img {
         width: auto;
       }
@@ -74,9 +81,24 @@ export default {
       justify-content: center;
       align-items: center;
       flex: 2;
+      @media screen and (max-width: 700px) {
+        justify-content: space-between;
+        margin: 12px 0;
+      }
       a {
         margin-right: 45px;
         color: #ccc;
+        @media screen and (max-width: 960px) {
+          margin-left: 25px;
+          margin-right: 0px;
+        }
+        @media screen and (max-width: 700px) {
+          margin-left: 0px;
+          font-size: 18px;
+        }
+        @media screen and (max-width: 500px) {
+          font-size: 15px;
+        }
         &:hover {
           color: #fff;
         }
@@ -87,6 +109,9 @@ export default {
       display: flex;
       justify-content: flex-end;
       align-items: center;
+      @media screen and (max-width: 960px) {
+        flex: 0 0 auto;
+      }
       .connect-icon {
         margin-left: 12px;
         width: 34px;
@@ -101,13 +126,23 @@ export default {
     .bottom-left {
       display: flex;
       align-items: center;
+      @media screen and (max-width: 700px) {
+        flex-direction: column;
+        align-items: flex-start;
+      }
       .footer-copy-right {
         color: #fff;
+        @media screen and (max-width: 700px) {
+          margin-bottom: 8px;
+        }
       }
       .links {
         margin-left: 34px;
         display: flex;
         align-items: center;
+        @media screen and (max-width: 700px) {
+          margin-left: 0px;
+        }
         a {
           margin-right: 20px;
           color: #ccc;
